0a424eddc36c858b9776348abc5a12b4053a1ddb
CodeSandbox <support@codesandbox.io>
2023-11-14 16:57:42 +0000

    Initial commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+// this layout will share a common component over the nested routes
+import React from "react";
+
+const SignIn = () => {
+    return (
+        <h1>
+            SignIn
+        </h1>
+    )
+}
+
+export default SignIn 
